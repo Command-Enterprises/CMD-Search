4bdcba71d3bf4988ccd003d383acb9b64d2131cd
Preston <preston.truong1@outlook.com>
2023-10-26 13:15:02 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-async function performSearch() {
-    const searchInput = document.getElementById("search-input").value;
+async function performSearch(query) {
     const resultsContainer = document.getElementById("search-results");
     resultsContainer.textContent = "Searching...";
 
+    const duckDuckGoEndpoint = `https://api.duckduckgo.com/?q=${encodeURIComponent(query)}&format=json`;
+
     try {
-        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${searchInput}`);
+        const response = await fetch(duckDuckGoEndpoint);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
         const data = await response.json();
 
-        if (data.length > 0) {
-            resultsContainer.innerHTML = data.map(post => `<div>${post.title}</div>`).join("");
+        if (data.Results && data.Results.length > 0) {
+            resultsContainer.innerHTML = data.Results.map(result => `<div><a href="${result.FirstURL}" target="_blank">${result.Text}</a></div>`).join("");
         } else {
             resultsContainer.textContent = "No results found.";
         }
@@ -16,12 +21,19 @@ async function performSearch() {
         console.error("Error fetching search results:", error);
         resultsContainer.textContent = "Error fetching search results.";
     }
+
+    history.pushState({}, "", `/q=${encodeURIComponent(query)}`);
 }
 
-document.getElementById("search-button").addEventListener("click", performSearch);
-
-document.getElementById("search-input").addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        performSearch();
-    }
+document.getElementById("search-form").addEventListener("submit", function(event) {
+    event.preventDefault(); 
+    const query = document.getElementById("search-input").value;
+    performSearch(query);
 });
+
+const urlParams = new URLSearchParams(window.location.search);
+const queryParam = urlParams.get('q');
+if (queryParam) {
+    document.getElementById("search-input").value = queryParam;
+    performSearch(queryParam);
+}
