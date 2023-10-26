f2084c84adc3e768ba496add5dc722a0303be6e4
Preston <preston.truong1@outlook.com>
2023-10-26 12:41:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function performSearch() {
+async function performSearch() {
     const searchInput = document.getElementById("search-input").value;
     const resultsContainer = document.getElementById("search-results");
-    // Replace this with your actual search logic, for now, displaying the input value as a result.
-    resultsContainer.textContent = `Search results for: ${searchInput}`;
+    resultsContainer.textContent = "Searching...";
+
+    try {
+        const response = await fetch(`https://jsonplaceholder.typicode.com/posts?q=${searchInput}`);
+        const data = await response.json();
+
+        if (data.length > 0) {
+            resultsContainer.innerHTML = data.map(post => `<div>${post.title}</div>`).join("");
+        } else {
+            resultsContainer.textContent = "No results found.";
+        }
+    } catch (error) {
+        console.error("Error fetching search results:", error);
+        resultsContainer.textContent = "Error fetching search results.";
+    }
 }
 
 document.getElementById("search-button").addEventListener("click", performSearch);
+
+document.getElementById("search-input").addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        performSearch();
+    }
+});
