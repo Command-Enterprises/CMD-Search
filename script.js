83eddd63193db87b910a8dc5e1c8c3f94d0034dc
Preston <preston.truong1@outlook.com>
2023-10-27 10:29:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,32 +5,32 @@ async function performSearch(query) {
     const duckDuckGoEndpoint = `https://api.duckduckgo.com/?q=${encodeURIComponent(query)}&format=json`;
 
     try {
-        console.log("API Endpoint:", duckDuckGoEndpoint); // Log API endpoint URL
         const response = await fetch(duckDuckGoEndpoint);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
-        console.log("API Response:", data); // Log API response data
 
         if (data.Results && data.Results.length > 0) {
-            resultsContainer.innerHTML = data.Results.map(result => `<div><a href="${result.FirstURL}" target="_blank">${result.Text}</a></div>`).join("");
+            resultsContainer.innerHTML = data.Results.map(result => {
+                const iconURL = result.Icon && result.Icon.URL ? result.Icon.URL : 'https://example.com/default-icon.png'; // Replace with your default icon URL
+                return `
+                    <div class="result-item">
+                        <img class="result-icon" src="${iconURL}" alt="Result Icon">
+                        <div class="result-details">
+                            <a href="${result.FirstURL}" target="_blank" class="result-title">${result.Text}</a>
+                            <p class="result-url">${result.FirstURL}</p>
+                        </div>
+                    </div>
+                `;
+            }).join("");
         } else {
             resultsContainer.textContent = "No results found.";
         }
     } catch (error) {
         console.error("Error fetching search results:", error);
         resultsContainer.textContent = "Error fetching search results.";
-    }
-}
-
-function loadResultsFromQuery() {
-    const urlParams = new URLSearchParams(window.location.search);
-    const queryParam = urlParams.get('q');
-    if (queryParam) {
-        document.getElementById("search-input").value = queryParam;
-        performSearch(queryParam);
     }
 }
 
@@ -41,4 +41,10 @@ document.getElementById("search-form").addEventListener("submit", function(event
     history.pushState({}, "", `/search?q=${encodeURIComponent(query)}`);
 });
 
-loadResultsFromQuery();
+// Load results if query is present in the URL
+const urlParams = new URLSearchParams(window.location.search);
+const queryParam = urlParams.get('q');
+if (queryParam) {
+    document.getElementById("search-input").value = queryParam;
+    performSearch(queryParam);
+}
