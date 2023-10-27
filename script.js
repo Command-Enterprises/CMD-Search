e0257e08e81f6ec8592d9f348d56f0b623e9f51f
harveyay <83598795+harveyay@users.noreply.github.com>
2023-10-27 11:32:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,27 +2,29 @@ const container = document.querySelector(".container");
 const searchInput = document.getElementById("search-input");
 const searchForm = document.getElementById("search-form");
 const searchResults = document.getElementById("search-results");
+const googleApiKey = 'YOUR_GOOGLE_API_KEY'; // Replace with your Google API key
+const cx = 'YOUR_CX_VALUE'; // Replace with your Custom Search Engine ID (CX)
 
 async function performSearch(query) {
     searchResults.textContent = "Searching...";
 
-    const duckDuckGoEndpoint = `https://api.duckduckgo.com/?q=${encodeURIComponent(query)}&format=json`;
+    const googleSearchEndpoint = `https://www.googleapis.com/customsearch/v1?key=${googleApiKey}&cx=${cx}&q=${encodeURIComponent(query)}`;
 
     try {
-        const response = await fetch(duckDuckGoEndpoint);
+        const response = await fetch(googleSearchEndpoint);
         if (!response.ok) {
             throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
 
-        if (data.Results && data.Results.length > 0) {
-            searchResults.innerHTML = data.Results.map(result => {
+        if (data.items && data.items.length > 0) {
+            searchResults.innerHTML = data.items.map(result => {
                 return `
                     <div class="result-item">
                         <div class="result-text">
-                            <a href="${result.FirstURL}" class="result-title" target="_blank">${result.Text}</a>
-                            <p class="result-link">${result.FirstURL}</p>
+                            <a href="${result.link}" class="result-title" target="_blank">${result.title}</a>
+                            <p class="result-link">${result.link}</p>
                         </div>
                     </div>
                 `;
@@ -42,3 +44,4 @@ searchForm.addEventListener("submit", async function(event) {
     await performSearch(query);
     history.pushState({}, "", `/search?q=${encodeURIComponent(query)}`);
 });
+
